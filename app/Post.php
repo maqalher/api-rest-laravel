5bc94bd1b8060476e069371974176dff77daced9
maqalher <maqalher@hotmail.com>
2021-04-03 01:33:02 -0600

    fixes Model Post

--- app/Post.php
+++ app/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
     protected $tale = 'posts';
 
     protected $fillable = [
-        'title', 'content', 'category_id'
+        'title', 'content', 'category_id', 'image'
     ];
 
     // Relacion de uno a muchos inversa (muchos a uno)
